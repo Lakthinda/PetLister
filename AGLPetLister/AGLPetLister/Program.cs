82096746b3c9fe2c695685c6120add57313683ce
Lak Ranasinghe <lakthinda@gmail.com>
2020-05-26 17:15:24 +1000

    Initial .Net core project creation

---
+++ AGLPetLister/AGLPetLister/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AGLPetLister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
